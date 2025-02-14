4fd51f1358875d7ee22408ebe2e40c3c47eebe89
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2025-02-14 15:57:46 -0800

    updated BallControl as per documentation

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,11 +1,27 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BallController : MonoBehaviour
 {
+
+    [SerializeField] private float force = 1f;
+    [SerializeField] private InputManager inputManager;
+
+    private Rigidbody ballRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //reference to the RigidBody
+        ballRB = GetComponent<Rigidbody>();
+
+        //Add a listener to OnSpacePressed event
+        //when space pressed, LaunchBall called
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+    }
+
+    private void LaunchBall()
+    {
+        ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
     // Update is called once per frame
