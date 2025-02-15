582fde711be3daa03142a3c09056bd349121a9f6
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2025-02-14 17:02:08 -0800

    added Gutter script and updated as per documentation. changes force on ball to send down the gutters.

---
+++ Assets/Scripts/Gutter.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Gutter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider triggeredBody)
+    {
+        //rigidbody of the ball
+        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+
+        //store before resetting the velocity
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+
+        //reset linear AND angular velocity as ball is rotating on the ground
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.angularVelocity = Vector3.zero;
+
+        //add force for the gutter to change direction
+        ballRigidBody.AddForce(transform.forward *  velocityMagnitude, ForceMode.VelocityChange);
+    }
+}
